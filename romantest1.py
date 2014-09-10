1c81cc8eea45394acaaed8a86a0f78aedf57d613
oyuFuentes <Oyuki Fuentes>
2014-09-09 20:07:31 -0500

    Prueba 6
    Prueba donde debe fallar en escenarios de números naturales como 0 y
    numero negativo como -1

--- romantest1.py
+++ romantest1.py
@@ -67,11 +67,19 @@ class KnownValues(unittest.TestCase):
          result = roman.to_roman(integer)       
          self.assertEqual(numeral, result) 
 
-         
+
 class ToRomanBadInput(unittest.TestCase):                                 
-   def test_too_large(self):                                             
-      '''to_roman should fail with large input'''
-      self.assertRaises(roman.OutOfRangeError, roman.to_roman, 4000)
+	def test_too_large(self):                                             
+		'''to_roman should fail with large input'''
+		self.assertRaises(roman.OutOfRangeError, roman.to_roman, 4000)
+
+	def test_zero(self):
+		'''to_roman should fail with 0 input'''
+		self.assertRaises(roman.OutOfRangeError, roman.to_roman, 0)     
+
+	def test_negative(self):
+		'''to_roman should fail with negative input'''
+		self.assertRaises(roman.OutOfRangeError, roman.to_roman, -1)
 
 if __name__ == '__main__':
     unittest.main()
