17790d227c5a87656c0455f1d48d2fd6e10b361c
oyuFuentes <Oyuki Fuentes>
2014-09-09 19:58:46 -0500

    Prueba 2
    Prueba debe pasar con el creación de código.

--- roman.py
+++ roman.py
@@ -1,7 +1,24 @@
-
+roman_numeral_map = (('M',  1000),
+					('CM', 900),
+					('D',  500),
+					('CD', 400),
+					('C',  100),
+					('XC', 90),
+					('L',  50),
+					('XL', 40),
+					('X',  10),
+					('IX', 9),
+					('V',  5),
+					('IV', 4),
+					('I',  1)) 
 
 def to_roman(numero_entero):
 	"""
 	TDD
 	"""
-	pass
+	result = ''
+	for numeral, integer in roman_numeral_map:
+		while numero_entero >= integer:                     
+			result += numeral
+			numero_entero -= integer
+	return result
